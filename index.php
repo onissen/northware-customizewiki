3b8b2056b7f18938c7cbb4c45d27b18fdf4d4a2f
Oke <56229919+onissen@users.noreply.github.com>
2022-12-17 23:59:01 +0100

    All Northware Custumization settings here

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
+    # Disable Discussions
+    $wgHooks['SkinTemplateNavigation'][] = 'replaceTabs';
+    function replaceTabs( $skin, &$links) {  
+            unset( $links['namespaces']['talk'] ); // Remove the talk action
+            return true;
+    }
+ 
+
+    # Category Sidebar
+    $wgCategoryTreeSidebarRoot = 'Category:DisplaySidebar';
+
     include 'grouprights.php';
 ?>
