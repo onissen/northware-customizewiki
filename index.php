86bea6ff598acaf29aad5d5d26dc804c12145c04
Oke <56229919+onissen@users.noreply.github.com>
2022-12-18 00:02:44 +0100

    Disable discussions over replaceTabs is depreached

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?php
-    # Disable Discussions
-    $wgHooks['SkinTemplateNavigation'][] = 'replaceTabs';
-    function replaceTabs( $skin, &$links) {  
-            unset( $links['namespaces']['talk'] ); // Remove the talk action
-            return true;
-    }
- 
-
     # Category Sidebar
     $wgCategoryTreeSidebarRoot = 'Category:DisplaySidebar';
 
